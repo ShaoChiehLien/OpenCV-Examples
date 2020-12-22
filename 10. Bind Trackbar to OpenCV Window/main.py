1d6ae2e367c5fb1cba6f7c8eb956ba77825cff41
Shao-Chieh Lien <liens@purdue.edu>
2020-12-22 10:49:59 -0500

    added example 11: Object Detection and Object Tracking

--- 10. Bind Trackbar to OpenCV Window/main.py
+++ 10. Bind Trackbar to OpenCV Window/main.py
@@ -1,2 +1,45 @@
-import cv2 as cv
+import numpy as np
+import cv2
 
+
+def nothing(x):
+    print(x)
+
+
+cv2.namedWindow('image')  # create a window named 'image'
+
+# add trackbars named 'R', 'G', 'B', range from 0 to 255 on window 'image'
+cv2.createTrackbar('R', 'image', 0, 255, nothing)
+cv2.createTrackbar('G', 'image', 0, 255, nothing)
+cv2.createTrackbar('B', 'image', 0, 255, nothing)
+# add a trackbar named 'gray', range from 0 to 1 on window 'image'
+cv2.createTrackbar('gray', 'image', 0, 1, nothing)
+
+while True:
+    # must read the original colored image, because cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # could only convert colored to gray. Gray to gray would show an error
+    img = cv2.imread('lena.jpg')
+
+    # Create img1 with the BGR value that read from the trackbar
+    img1 = np.zeros(img.shape, np.uint8)
+    img1[:, :, 0] = cv2.getTrackbarPos('B', 'image')
+    img1[:, :, 1] = cv2.getTrackbarPos('G', 'image')
+    img1[:, :, 2] = cv2.getTrackbarPos('R', 'image')
+
+    gray = cv2.getTrackbarPos('gray', 'image')
+
+    key = cv2.waitKey(1)
+
+    if key == 27:  # check if 'esc' is pressed
+        break
+
+    if gray == 0:  # check if gray scale is off
+        img = cv2.add(img, img1)  # add the original colored image with the BGR value from trackbar
+    else:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # convert the original colored image to grayscale
+        # Note: cv2.cvtColor(var, cv2.COLOR_BGR2GRAY), could only convert COLORED image to grayscale
+        # can't convert an already grayscale image to grayscale
+
+    img = cv2.imshow('image', img)
+
+cv2.destroyAllWindows()
---
+++ 11. Object Detection and Object Tracking Using HSV Color Space/main.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+
+
+def nothing(x):
+    pass
+
+
+cap = cv2.VideoCapture(0)  # Access the computer camera
+
+#  Create trackbar for lower and upper bound of HSV
+cv2.namedWindow('Tracking Image')
+cv2.createTrackbar('LH', 'Tracking Image', 0, 255, nothing)
+cv2.createTrackbar('LS', 'Tracking Image', 43, 255, nothing)
+cv2.createTrackbar('LV', 'Tracking Image', 107, 255, nothing)
+cv2.createTrackbar('UH', 'Tracking Image', 255, 255, nothing)
+cv2.createTrackbar('US', 'Tracking Image', 255, 255, nothing)
+cv2.createTrackbar('UV', 'Tracking Image', 255, 255, nothing)
+
+while True:
+    _, frame = cap.read()
+    frame = cv2.resize(frame, (640, 360))
+
+    # convert the frame's color storage method from BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # get the current value of HSV bound
+    l_h = cv2.getTrackbarPos('LH', 'Tracking Image')
+    l_s = cv2.getTrackbarPos('LS', 'Tracking Image')
+    l_v = cv2.getTrackbarPos('LV', 'Tracking Image')
+    u_h = cv2.getTrackbarPos('UH', 'Tracking Image')
+    u_s = cv2.getTrackbarPos('US', 'Tracking Image')
+    u_v = cv2.getTrackbarPos('UV', 'Tracking Image')
+
+    # set up an array contains the lower and upper bound of the HSV value
+    l_b = np.array([l_h, l_s, l_v])
+    u_b = np.array([u_h, u_s, u_v])
+
+    # create a mask that could mask out the colors that are out of the HSV range
+    mask = cv2.inRange(hsv, l_b, u_b)
+
+    # use bitwise_and to do the mask
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+    cv2.imshow('frame', frame)  # show the original frame
+    cv2.imshow('mask', mask)  # show the mask, only the color that is in the HSV boundary would be white
+    cv2.imshow('res', res)  # show the result
+
+    key = cv2.waitKey(1)
+
+    if key == 27:  # if exc is pressed, exit the loop
+        break
+
+cap.release()
+cv2.destroyAllWindows()
